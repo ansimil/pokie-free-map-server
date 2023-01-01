83690b2677dcbc3497bade2413fd40516334d786
John Doe <andrewsimonmiller@gmail.com>
2023-01-02 09:52:32 +1100

    implemented captcha route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
+const axios = require('axios')
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+
+router.post("/validatecaptcha", async (req, res) => {
+      const {token} = req.body;
+      await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET_KEY}&response=${token}`
+        );
+  
+        if (res.status(200)) {
+          res.send("Good");
+      }else{
+        res.send("Robot");
+      }
+  });
 
 module.exports = router;
